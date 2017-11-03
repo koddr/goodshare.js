1b863019f0b61567dcc2c52fc813a9dfd473ac0a
Vic Shóstak <koddr@users.noreply.github.com>
2017-11-03 10:46:14 +0300

    Update GooglePlus.js

--- src/providers/GooglePlus.js
+++ src/providers/GooglePlus.js
@@ -26,36 +26,6 @@ class GooglePlus {
             });
         });
     }
-    
-    getCounter() {
-        const script = document.createElement('script');
-        const callback = ('goodshare_' + Math.random()).replace('.', '');
-        const count_elements = document.querySelectorAll('[data-counter=googleplus]');
-        const count_url = 'https://query.yahooapis.com/v1/public/yql?q='
-            + encodeURIComponent('select * from html where url="https://plusone.google.com/_/+1/fastbutton?url='
-                + this.url + '" and xpath="*"') + '&callback=' + callback;
-        
-        if (count_elements.length > 0) {
-            window[callback] = (counter) => {
-                [...count_elements].forEach((item) => {
-                    if (counter.results[0]) {
-                        item.innerHTML = ((counter.results[0]).match(/javascript">window.__SSR = \{c: (\d+).0/) !== null)
-                            ? (counter.results[0]).match(/javascript">window.__SSR = \{c: (\d+).0/)[1] / 1
-                            : 0;
-                    }
-                    else {
-                        item.innerHTML = 0;
-                    }
-                });
-                
-                script.parentNode.removeChild(script);
-            };
-            
-            script.src = count_url;
-            document.body.appendChild(script);
-        }
-    }
 }
 
 export const googleplus_share = new GooglePlus().shareWindow();
-export const googleplus_counter = new GooglePlus().getCounter();
