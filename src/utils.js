1caa9388c608ae11c9fb601d5024434ad096ee1c
Ilya Reshetnikov <devxom@gmail.com>
2018-05-22 21:11:20 +0300

    Add utils for renew mechanism
    Methods:
     - wrapper for target and remove events
     - pseudo uniqId method

---
+++ src/utils.js
@@ -0,0 +1,42 @@
+export const getUniqId = (prefix) =>
+  `${prefix}-${Math.random().toString(36).substr(2, 8)}`;
+
+export class EventWithNamespace {
+  constructor() {
+    this.handlers = {};
+  }
+
+  addEventListener(
+    target = document,
+    eventWithNamespace = 'click',
+    func
+  ) {
+    this.handlers[eventWithNamespace] = {
+      func: func,
+      target: target
+    };
+
+    const eventType = eventWithNamespace.split('.')[0];
+    const eventHandler = this.handlers[eventWithNamespace]['func'];
+
+    target.addEventListener(eventType, eventHandler);
+  }
+
+  removeEventListener(
+    event = 'click',
+  ) {
+    const eventType = event.split('.')[0];
+    const eventData = this.handlers[event];
+    const target = eventData.target;
+
+    target.removeEventListener(eventType, eventData.func);
+
+    delete this.handlers[event];
+  }
+
+  removeAll() {
+    for (const key in this.handlers) {
+      this.removeEventListener(key);
+    }
+  }
+}
